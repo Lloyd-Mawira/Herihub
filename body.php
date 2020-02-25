b8f71bd5f312adc33bb598170d18625825ab4d16
Lloyd Mawira <39905364+Lloyd-Mawira@users.noreply.github.com>
2020-02-25 10:54:48 +0300

    Add files via upload

---
+++ body.php
@@ -0,0 +1,49 @@
+
+
+<div class="col-sm-12 col-xs-12 no_padding">
+
+<div class="col-sm-8 col-xs-12 no_padding">
+	
+<div class="col-xs-12 col-sm-12 " id="slider"></div>
+
+<div class="col-xs-12 col-sm-12 no_padding mobile_hidden" id="continous_div"></div>
+<!--<div class="col-xs-12 col-sm-12 mobile_hidden  " id="cont_div"><span class=" cont_div btn theme_bg " id="">Click to Submit Innovation</span></div>-->
+</div>
+<div class="col-sm-4  col-xs-12" >
+<div class="col-sm-12 no_padding col-xs-12" id="nmk_info">
+   <div class="col-sm-12 col-xs-12 no_padding">
+   	<div class="col-xs-12 col-sm-12" id='about_nmk_header'><strong>The National Museum of Kenya</strong></div>
+   	<div class="col-xs-12 col-sm-12 no_padding font_justify"> 
+
+National Museums of Kenya Innovation Portal is a product of National Museums of Kenya. It is a platform that seeks to bridge the gap between innovators and implementers. The portal identifies innovations and ideas that have high potential impact and links them to implementers who are seeking sustainable solutions.
+</div>
+<div class="col-sm-12 col-xs-12 no_padding font_justify">
+	<br>
+National Museums of Kenya seeks to contribute positively to the National development of Kenya by promoting the preservation of the Kenyan natural and cultural heritage. The Innovation portal invites the public to share innovations that are driving forward the agenda on biodiversity conservation and providing solutions that improve and meet the growing needs of the society. 
+</div>
+<div class="col-xs-12 col-sm-12 " id="cont_div"><br><span class=" cont_div btn theme_bg " id="">Click to Submit Innovation</span></div>
+   </div>
+  
+<!--<div class="col-sm-12 no_padding" id="g_map">
+	<div class="col-sm-12 no_padding" id="nmk_map"><span >Map</span></div>
+	<div class="col-sm-12 no_padding">
+	<iframe class="col-xs-12 col-sm-12" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63820.97742734887!2d36.83421966879469!3d-1.2874630621516925!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f172e2b30a483%3A0x24eee4c08364a0f3!2sNairobi%20National%20Museum!5e0!3m2!1sen!2ske!4v1568210857940!5m2!1sen!2ske" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+	</div>
+</div>-->
+</div>
+
+</div>
+</div>
+<script type="text/javascript"  src="js/scripts/body.js"></script>
+
+
+<script type="text/javascript">
+	$(document).ready(function(){
+	    //disabling right clicking
+	     $(document).bind("contextmenu",function(e){
+        
+        return false;
+        
+    })
+    </script>
---
+++ footer.php
@@ -0,0 +1,79 @@
+
+	<div class="col-sm-12 col-xs-12 mobile_hidden">
+		<div class="col-sm-3 footer_divs col-xs-12  ">
+				<div class="col-sm-12 col-xs-12 no_padding" id="useful_links">Useful Links</div>
+       <div class="col-sm-12 col-xs-12 useful_link" onclick=" window.open('http://kenyalaw.org:8181/exist/kenyalex/actview.xql?actid=Const2010')" id="">Constitution of Kenya</div>
+           
+      <div class="col-sm-12 col-xs-12 useful_link"  onclick=" window.open('http://kenyalaw.org:8181/exist/kenyalex/actview.xql?actid=No.%206%20of%202006')" id="">National Heritage Act No.6 of 2006</div>
+          
+				<div class="col-sm-12 col-xs-12 useful_link"  onclick=" window.open('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=3&ved=2ahUKEwjiuP_HtNzkAhWMzoUKHalkCh8QFjACegQIABAC&url=http%3A%2F%2Fwww.kenyabrussels.com%2Fckfinder%2Fuserfiles%2Ffiles%2Fabout%2520Kenya%2Ftourist%2520brochures%2FCradle_of_Mankind.pdf&usg=AOvVaw1sd_93t3wnhr9Pl7WBlS37')" id="">Kenya Truly The Cradle of Humankind</div>
+        <div class="col-sm-12 col-xs-12 useful_link"  onclick=" window.open('https://www.kipi.go.ke/')" id="">Kenya Industrial Property Institute</div>
+           </div>
+
+  <div class="col-sm-3 footer_divs col-xs-12 ">
+        <div class="col-sm-12 col-xs-12 no_padding" id="useful_links">Our Contacts</div>
+       <div class="col-sm-12 col-xs-12 useful_link" id="">Email:info@nmkinnovations.co.ke</div>
+           
+      <div class="col-sm-12 col-xs-12 useful_link" id="">Tel: +254-20-8164134</div>
+          
+        <div class="col-sm-12 col-xs-12 useful_link" id="">P.O. BOX 40658- 00100
+Nairobi,Kenya.</div>
+           </div>
+
+ <div class="col-sm-3 footer_divs col-xs-12 ">
+        <div class="col-sm-12 col-xs-12 no_padding" id="useful_links">Our Partners</div>
+
+        
+       <div class="col-sm-5 col-xs-12 " style="background: #fff; margin-top: 7px;">
+       <div class="col-sm-12 col-xs-7  icta" style="background:url(images/icons/Icta.png)"></div>
+     </div>
+		</div>
+
+
+
+
+		 <div class="col-sm-3  col-xs-12  ">
+        <div class="col-sm-12 col-xs-12 no_padding" id="useful_links">Social Media Links</div>
+        <div class="col-xs-12 col-sm-12">&nbsp;</div>
+       <div class="col-sm-6 no_padding col-xs-12">
+<div class="col-sm-1 col-xs-2 media" style="background:url(images/icons/facebook.png)"></div>
+        <div class="col-sm-9 col-xs-10 useful_link" id="">Facebook</div>
+         </div>
+         <div class="col-sm-6 no_padding col-xs-12">
+          <div class="col-sm-1 col-xs-2 media" style="background:url(images/icons/linked.png)"></div>
+        <div class="col-sm-9 col-xs-10 useful_link" id="">Linkedin</div>
+           </div>
+           <div class="col-sm-6 no_padding col-xs-12">
+             <div class="col-sm-1 col-xs-2 media" style="background:url(images/icons/insta.jpeg)"></div>
+        <div class="col-sm-9 col-xs-8 useful_link" id="">Instagram</div>
+        </div>
+        <div class="col-sm-6 no_padding col-xs-12">
+         <div class="col-sm-1 col-xs-2 media" style="background:url(images/icons/tweeter.png)"></div>
+        <div class="col-sm-9 col-xs-10 useful_link" id="">Twitter </div>
+           </div>
+         </div>
+         
+</div>
+    <div class="col-sm-12 col-xs-12 desktop_hidden mobile_hidden">
+  <div class=" footer_divs col-xs-6 ">
+        <div class="col-sm-12 col-xs-12 no_padding" id="useful_links">Our Partners</div>
+
+        
+       <div class="col-sm-5 col-xs-12 " style="background: #fff; margin-top: 7px;">
+       <div class="col-sm-12 col-xs-7  icta" style="background:url(images/icons/Icta.png)"></div>
+     </div>
+    </div>
+
+ <div class=" col-xs-6 ">
+        <div class="col-sm-12 col-xs-12 no_padding" id="useful_links">Our Contacts</div>
+       <div class="col-sm-12 col-xs-12 mobile_hidden useful_linked" id="">Email:info@nmkinnovations.co.ke</div>
+           
+      <div class="col-sm-12 col-xs-12 useful_linked" id="">Tel: +254-20-8164134</div>
+          
+        <div class="col-sm-12 col-xs-12 useful_linked" id="">P.O. BOX 40658- 00100
+Nairobi,Kenya.</div>
+           </div>
+    </div>
+    <div class="col-sm-12 col-xs-12 " id="lowered_foot">
+  Powered by the ICT Authority. All Rights Reserved.
+           </div>
