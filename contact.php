d81e7c41338f7952360f61df78470303cb15f8be
Lloyd Mawira <39905364+Lloyd-Mawira@users.noreply.github.com>
2020-02-25 10:56:45 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,338 @@
+<style type="text/css">
+	#contact_heads{
+		text-align: center;
+	}
+	#id_contacts{
+		box-shadow: 0 0 3px #ccc;
+		background:#f2f2f2;;
+	}
+	#btn_kgh{
+		text-align: right;
+	}
+	#send_mesd{
+		background:#e6b234;
+		margin-bottom: 10px;
+	}
+	.contacts_wrapper{
+		min-height: 200px; 
+		box-shadow: 0 0 2px #ccc;
+		margin-top: 10px;
+		border-radius: 10px;
+padding-bottom: 10px;
+
+	}
+	#contax{
+	 font-weight: bolder;
+	 text-transform: capitalize;
+	 border-bottom: 1px dashed #000;
+	 margin-top: 4px;
+
+	}
+	.links_cont{
+		cursor: pointer;
+
+	}
+</style>
+
+
+<div class="col-sm-12 col-xs-12">
+	<div class="col-xs-12  col-sm-8" >
+		<div class="col-xs-12 contacts_wrapper col-sm-12">
+			<div class="col-sm-12 col-xs-12 no_padding">
+	<div class="col-sm-6 col-xs-12 ">
+
+
+<div class="col-sm-12 col-xs-12 no_padding" id="contax">National Museums of Kenya Corporate Headquarters</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Museum Hill</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">P.O. BOX 40658- 00100</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Nairobi,Kenya.</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Website: www.museums.or.ke</div>
+
+	</div>
+<div class="col-sm-6 col-xs-12 ">
+<div class="col-sm-12 col-xs-12 no_padding" id="contax">General Enquiries</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Public relations:-</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">E-mail: publicrelations@museums.or.ke</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Tel:+254-20-8164134/35/36</div>
+
+	</div>
+</div>
+<div class="col-sm-12 col-xs-12 no_padding">
+<div class="col-sm-6 col-xs-12 ">
+<div class="col-sm-12 col-xs-12 no_padding" id="contax">For Communication to the Director General</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Email: dgnmk@museums.or.ke</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Tel:+254-20-8164134/6</div>
+
+	</div>
+<div class="col-sm-6 col-xs-12 ">
+<div class="col-sm-12 col-xs-12 no_padding" id="contax">Membership</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Membership for Kenya Museum Society</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">+254724–255299 OR +254-20-2339158</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Email: info@kenyamuseumsociety.org</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">website: www.kenyamuseumsociety.org</div>
+</div>
+</div>
+<div class="col-sm-12 col-xs-12 no_padding">
+<div class="col-sm-6 col-xs-12 ">
+<div class="col-sm-12 col-xs-12 no_padding" id="contax">Ticket desk</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Booking and tickets</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">+254 20 8164136 ext 2335/ 2290</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">School Group Booking</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Email: nmkeduc@museums.or.ke</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Tel: +254 721 308485</div>
+
+</div>
+<div class="col-sm-6 col-xs-12 ">
+<div class="col-sm-12 col-xs-12 no_padding" id="contax">Feedback</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Comments, suggestions or concerns</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">E-mail: publicrelations@museums.or.ke</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">For complaints please write to us:</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">complaints@museums.or.ke</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Tel:+254-20-8164134/35/36</div>
+
+</div>
+</div>
+<div class="col-sm-12 col-xs-12 no_padding">
+<div class="col-sm-6 col-xs-12 ">
+<div class="col-sm-12 col-xs-12 no_padding" id="contax">Museum switchboard</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Tel: +254-20-8164134</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Tel: +254-20-8164135</div>
+<div class="col-sm-12 col-xs-12 links_cont" id="">Cell: +254721-308485</div>
+
+</div>
+</div>
+</div>
+
+	</div>
+<div class="col-xs-12  contacts_wrapper col-sm-4">
+	<div class="col-sm-12 col-xs-12 no_padding">
+	<div class="col-sm-12 col-xs-12 " id="contact_heads">
+	<h3>Contact us here</h3>
+    </div>
+    <div class="col-sm-12 col-xs-12 no_padding contacts_wrapper" id="id_contacts">
+
+		<div class="col-sm-12 col-xs-12">
+    Name:
+    </div>
+
+	<div class="col-sm-10 col-xs-10	">
+	<input class="splashinputs" type="text" placeholder="E.g. John Paul" id="name">
+    </div>
+	<div class="col-sm-2 col-xs-2 not_shown " id="name_check"><i class="fa fa-check"></i></div>
+
+		<div class="col-sm-12 col-xs-12">
+	Phone:
+     </div>
+	<div class="col-sm-10 col-xs-10	">
+	<input class="splashinputs" type="text" placeholder="E.g. 0712345678" id="phone">
+   </div>
+
+	<div class="col-sm-2 col-xs-2 not_shown " id="phone_check"><i class="fa fa-check"></i></div>
+
+
+		<div class="col-sm-12 col-xs-12">
+	Email:
+   </div>
+
+	<div class="col-sm-10 col-xs-10	">
+	<input class="splashinputs" type="email" placeholder="E.g. john@example.com" id="email">
+	</div>
+
+	<div class="col-sm-2 col-xs-2 not_shown " id="email_check"><i class="fa fa-check"></i></div>
+
+		<div class="col-sm-12 col-xs-12">
+	Message:
+</div>
+	<div class="col-sm-10 col-xs-10	">
+	<textarea class="splashinputs" id="message"></textarea> 
+	</div>
+
+	<div class="col-sm-2 col-xs-2 not_shown " id="message_check"><i class="fa fa-check"></i></div>
+
+	<div class="col-xs-12 col-sm-12" id="">
+		<div class="col-xs-12 col-sm-7 " id="erro_message">
+     </div>
+     <div class="col-xs-12 col-sm-5 ">
+	<span class="btn" id="send_mesd">Send message</span>
+</div>
+     </div>
+     	
+    </div>
+    </div>
+	</div>
+
+
+</div>
+<script type="text/javascript">
+	$(document).ready(function(){
+		var loader=$("#loader").html()
+		var action="#send_mesd";
+		$(action).click(function(){
+       
+		var targeted="#erro_message";
+		$(targeted).html(loader)
+		var message=btoa($("#message").val());
+		var url="modules/mails/send_email.php";
+		var urlret="modules/mails/reply_email.php";
+		var name=btoa($("#name").val())
+		var phone=btoa($("#phone").val())
+		var email=btoa($("#email").val());
+		if(email && message && name && phone){
+               if(email==""){
+
+
+         $("#email_check").hide();
+
+
+                	}else{
+//alert()
+var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    if(!emailReg.test(email)){
+        email_address="";
+        $("#email_address").val("");
+      $(".email_check").hide();
+        $("#email").attr("placeholder","Wrong email format!");
+       // $("#erro_message").html("Check your email!").css('color','red');
+       // $("#loader_error").html("Check your email Format!");
+    }else{
+    	//alert(email_address)
+ $("#email_check").show();
+ $("#erro_message").html("").css('color','red');
+    }
+//$(".cheked_pass").show();
+
+                	}
+
+$.post(""+url+"",{
+	email:email,
+	phone:phone,
+	message:message,
+	name:name
+
+},function(data){
+	if($.trim(data)=="Success"){
+$.post(""+urlret+"",{
+	email:email,
+	name:name
+
+},function(data){
+if($.trim(data)=="success"){
+	 $("#erro_message").html(data).css('color','red');
+}else{
+	 $("#erro_message").html("An error occured!").css('color','red');
+}
+})
+
+
+	
+	}else{
+	
+ $("#erro_message").html(responce).css('color','red');
+}
+})
+
+
+
+//alert("corect");
+
+
+
+
+}else{
+	$(targeted).html("All fields required!").css("color","red");
+}
+			//$("#email").focusout();
+
+	})
+		$("#name").focusin(function(){
+   $("#name_check").hide();
+   $("#name").val("");
+   $("#erro_message").html("").css('color','red');
+  });
+$("#name").focusout(function(){
+    var name=$("#name").val();
+               if(name==""){
+
+
+         $("#name_check").hide();
+
+
+                	}else{
+
+                		$("#name_check").show();
+                	}
+                })
+$("#phone").focusin(function(){
+   $("#phone_check").hide();
+   $("#phone").val("");
+   $("#erro_message").html("").css('color','red');
+  });
+$("#phone").focusout(function(){
+    var phone=$("#phone").val();
+               if(phone==""){
+
+
+         $("#phone_check").hide();
+
+
+                	}else{
+
+                		$("#phone_check").show();
+                	}
+                })
+
+$("#message").focusin(function(){
+   $("#message_check").hide();
+   $("#message").val("");
+   $("#erro_message").html("").css('color','red');
+  });
+$("#message").focusout(function(){
+    var message=$("#message").val();
+               if(message==""){
+
+
+         $("#message_check").hide();
+
+
+                	}else{
+
+                		$("#message_check").show();
+                	}
+                })
+
+
+$("#email").focusin(function(){
+   $("#email_check").hide();
+   $("#email").val("");
+   $("#erro_message").html("").css('color','red');
+  });
+ 
+$("#email").focusout(function(){
+    var email=$("#email").val();
+               if(email==""){
+
+
+         $("#email_check").hide();
+
+
+                	}else{
+//alert()
+var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    if(!emailReg.test(email)){
+        email_address="";
+        $("#email_address").val("");
+      $("#email_check").hide();
+        $("#email").attr("placeholder","Wrong email format!");
+        $("#erro_message").html("Check your email!").css('color','red');
+       // $("#loader_error").html("Check your email Format!");
+    }else{
+    	//alert(email_address)
+ $("#email_check").show();
+ $("#erro_message").html("").css('color','red');
+    }
+//$(".cheked_pass").show();
+
+                	}
+  });
+	})
+</script>
